844abe5cea3ded2a2ab7ea0e33c293f7f6489415
Ajit Thapa <ajitthapa248@gmail.com>
2025-03-04 09:56:22 +0545

    (chore): Disable mouse in nvim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -42,7 +42,7 @@ vim.opt.wildignore:append({ "*/node_modules/*" })
 vim.opt.splitbelow = true --Put new windows below current
 vim.opt.splitright = true --Put new windows right of current
 vim.opt.splitkeep = "cursor"
-vim.opt.mouse = "a"
+vim.opt.mouse = ""
 
 -- Cursor animation
 vim.opt.guicursor = {
