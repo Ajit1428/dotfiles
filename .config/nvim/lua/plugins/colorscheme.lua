00209529e86bc28be248a1236ab0523d154725bd
Ajit Thapa <ajitthapa248@gmail.com>
2025-04-21 15:23:48 +0545

    (update): Updated dotfiles

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
   lazy = false,
   config = function()
     require("catppuccin").setup({
-      flavour = "latte",
+      flavour = "mocha",
       term_colors = true,
       highlight_overrides = {
         mocha = function(colors)
