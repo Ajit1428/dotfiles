6d553cd9143e9c3848752494dd6e7d60d4a4e6b1
Ajit Thapa <ajitthapa248@gmail.com>
2025-05-11 13:20:53 +0545

    (chore): Updated the bg color

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,1 +1,13 @@
-return { "ellisonleao/gruvbox.nvim", priority = 1000, config = true }
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = function()
+    require("gruvbox").setup({
+      contrast = "hard", -- Set the contrast to hard
+      overrides = {
+        Normal = { bg = "#1d2021" }, -- Override Normal background to hard black
+      },
+    })
+    vim.cmd("colorscheme gruvbox")
+  end,
+}
