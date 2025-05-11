eabd92443983941ebfde1b0dc8f8b9723277645f
Ajit Thapa <ajitthapa248@gmail.com>
2025-05-11 12:57:46 +0545

    (update): Updated colorscheme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,24 @@
 # ╚═╝  ╚═╝   ╚═╝   
 # Fish config 2023
 
+set -g fish_color_normal normal
+set -g fish_color_command yellow
+set -g fish_color_keyword yellow
+set -g fish_color_quote green
+set -g fish_color_redirection red
+set -g fish_color_error red
+set -g fish_color_param white
+set -g fish_color_comment 555555
+set -g fish_color_match --background=brblack
+set -g fish_color_selection --background=brblack
+set -g fish_color_search_match --background=brblack
+set -g fish_color_operator cyan
+set -g fish_color_escape cyan
+set -g fish_color_autosuggestion 555555
+set -g fish_color_cwd blue
+set -g fish_color_user brgreen
+set -g fish_color_host normal
+
 if status is-interactive
     set fish_greeting "Welcome, AT sama 🐯"
     neofetch --kitty /home/at/Pictures/Wallpapers/LordShivaNeo.png
--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -9,7 +9,7 @@ vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", opts = { colorscheme = "catppuccin-mocha" }, import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim", opts = { colorscheme = "gruvbox" }, import = "lazyvim.plugins" },
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
