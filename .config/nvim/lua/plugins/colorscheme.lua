bdfaffd35b9582ceccb27515114d125df5c24d20
Ajit Thapa <ajitthapa248@gmail.com>
2025-05-14 20:19:31 +0545

    (chore): Reverted colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,31 @@
 return {
-  "ellisonleao/gruvbox.nvim",
+  "catppuccin/nvim",
+  name = "catppuccin",
   priority = 1000,
+  lazy = false,
   config = function()
-    require("gruvbox").setup({
-      contrast = "hard", -- Set the contrast to hard
-      overrides = {
-        Normal = { bg = "#1d2021" }, -- Override Normal background to hard black
+    require("catppuccin").setup({
+      flavour = "mocha",
+      term_colors = true,
+      highlight_overrides = {
+        mocha = function(colors)
+          return {
+            Comment = {
+              fg = "#00ff7f",
+            },
+            CursorLine = {
+              bg = colors.surface2,
+            },
+            Visual = {
+              bg = "#00A513",
+            },
+          }
+        end,
+      },
+      integrations = {
+        blink_cmp = true,
+        snacks = true,
       },
     })
-    vim.cmd("colorscheme gruvbox")
   end,
 }
